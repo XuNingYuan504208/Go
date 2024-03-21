59b8072004361ac9aba8353ee68073a18af0ac0f
li.xu <li.xu@gomap.ae>
2024-03-21 21:16:37 +0800

    variable

--- src/Demo.go
+++ src/Demo.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func goFunc(i int) {
@@ -10,9 +9,11 @@ func goFunc(i int) {
 	fmt.Println("i:", i)
 }
 
-func main() {
-	for i := 0; i < 1000; i++ {
+func main1() {
+	/*for i := 0; i < 1000; i++ {
 		go goFunc(i) // go 关键字开启协程
 	}
-	time.Sleep(time.Second)
+	time.Sleep(time.Second)*/
+	//Var.declare()
+
 }
---
+++ src/Var.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+var staticA int = 100
+
+var staticB = 200
+
+// := 初始化方式只能在函数中使用
+// staticC := 500;
+
+func declare() {
+	var a int = 1
+	var b = 1
+
+	c := 1
+
+	fmt.Println("a:", a, "b:", b, "c:", c)
+	fmt.Println("staticA:", staticA, "staticB:", staticB, "c:", c)
+
+	// 多变量声明
+}
+
+func main() {
+	declare()
+}
